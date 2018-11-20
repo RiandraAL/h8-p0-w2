d8347b311c3b8437e7c325e271c82b63dd384f47
Riandra <riandra.alkautsar@yahoo.com>
2018-11-20 23:14:43 +0700

    menambah file exercise-3.js

--- exercise-3.js
+++ exercise-3.js
@@ -4,24 +4,34 @@ var nama = 'Mikael';
 var peran = '';
 */
 
-var nama = 'andra';
-var peran = '';
 
-if (nama === '' || peran !== '') {console.log('Nama harus diisi!');} 
+var nama=prompt("Masukkan Nama Anda:");
+var peran=prompt("Pilih Peran Anda (Ksatria/Tabib/Penyihir)");
+if(nama===''){
+  console.log("Nama Harus Diisi");
+  if(peran===''){
+    console.log("Pilih peranmu untuk memulai game")
+  }
+}
+else{
+  if(peran==="Ksatria"||peran=="ksatria"){
+  console.log("Selamat datang di Dunia Proxytia "+nama);
+  console.log("Halo Ksatria "+nama+", kamu dapat menyerang dengan senjatamu!");
+  }
+  else if(peran==="Tabib"||peran=="tabib"){
+  console.log("Selamat datang di Dunia Proxytia "+nama);
+  console.log("Halo Tabib "+nama+", kamu akan membantu temanmu yang terluka");
+  }
+  else if(peran==="Penyihir"||peran=="penyihir"){
+  console.log("Selamat datang di Dunia Proxytia "+nama);
+  console.log("Halo Penyihir "+nama+", ciptakan keajaiban yang membantu kemenanganmu!");
+  }
+  else{
+    console.log("Selamat datang di Dunia Proxytia "+nama);
+    console.log("Peranmu tidak boleh kosong");
+  }
+}
 
-else if (nama === 'andra' && peran === '') {console.log('Halo Andra, pilih peranmu untuk memulai game!');}
-
-else if (nama === 'nina' && peran === 'ksatria')
-{console.log('Selamat datang di Dunia Proxytia, nina.');
-('Halo Ksatria nina, kamu dapat menyerang dengan senjatamu!');}
-
-else if (nama == 'danu' && peran == 'tabib')
-{console.log('Selamat datang di Dunia Proxytia, Danu');
-('Halo Tabib Danu, kamu akan membantu temanmu yang terluka.');}
-
-else if (nama == 'zero' && peran == 'penyihir')
-{console.log('Selamat datang di Dunia Proxytia, Zero');
-('Halo Penyihir Zero, ciptakan keajaiban yang membantu kemenanganmu!');}
 
 /* 
 Output untuk Input nama = '' dan peran = ''
